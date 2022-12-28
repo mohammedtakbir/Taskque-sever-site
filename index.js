05878101d05125ad48929a0f07bfbca1bed6f165
mohammedtakbir <takbirhossain678@gmail.com>
2022-12-28 14:21:20 +0600

    created API for update the user task

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 //* middleware
 app.use(cors());
@@ -28,6 +28,14 @@ async function run() {
             res.send(result);
         })
 
+        //* load task for update
+        app.get('/task/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const task = await AddTaskCollection.findOne(query);
+            res.send(task)
+        })
+
         //* get tasks using email
         app.get('/myTask', async (req, res) => {
             const email = req.query.email;
@@ -35,6 +43,29 @@ async function run() {
             const myTasks = await AddTaskCollection.find(query).toArray();
             res.send(myTasks);
         })
+
+
+        //* update a task
+        app.patch('/updatedTask/:id', async (req, res) => {
+            const updatedTask = req.body;
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    taskTitle: updatedTask.taskTitle,
+                    taskDetail: updatedTask.taskDetail,
+                    taskImage: updatedTask.taskImage
+                }
+            };
+            const result = await AddTaskCollection.updateOne(filter, updatedDoc, options);
+            res.send(result)
+        })
+
+
+
+
+
     }
     finally {
 
