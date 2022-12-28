eb87d9d34b90646dbe383b5e46d1666b6dbb9fc4
mohammedtakbir <takbirhossain678@gmail.com>
2022-12-28 16:44:08 +0600

    create a delete API for deleting user task

--- index.js
+++ index.js
@@ -62,6 +62,16 @@ async function run() {
             res.send(result)
         })
 
+        //* delete a task
+        app.delete('/deleteTask/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await AddTaskCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
+
 
 
 
