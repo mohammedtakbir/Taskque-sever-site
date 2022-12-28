eee31235fe8418e50ab566ee8b55df4f2d9b7f74
mohammedtakbir <takbirhossain678@gmail.com>
2022-12-28 11:38:37 +0600

    create an API for added task

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 //* middleware
 app.use(cors());
@@ -10,6 +12,40 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Taskque server is running');
 })
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.drjbcpx.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const AddTaskCollection = client.db('Taskque').collection('addTasks');
+
+        //* add a task
+        app.post('/addTask', async (req, res) => {
+            const task = req.body;
+            const result = await AddTaskCollection.insertOne(task);
+            res.send(result);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.log(err))
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(port, () => {
     console.log(`Taskque server is running on ${port} port`)
